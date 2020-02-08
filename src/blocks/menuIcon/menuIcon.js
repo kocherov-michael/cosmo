ee2ba8b1303b91e024baec135dcd5ea266d0c57b
Michael <ko4erov@mail.ru>
2020-02-08 23:23:54 +0300

    menu icon animation and menu list ready

--- src/blocks/menuIcon/menuIcon.js
+++ src/blocks/menuIcon/menuIcon.js
@@ -1,1 +1,10 @@
 import './menuIcon.scss'
+
+const menuIconElement = document.querySelector('[data-menu-icon]')
+const menuElement = document.querySelector('[data-menu]')
+
+menuIconElement.addEventListener('click', () => {
+	// console.log('push')
+	menuElement.classList.toggle('menu--show')
+	menuIconElement.classList.toggle('menu-icon--cross')
+})
