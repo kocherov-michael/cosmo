d9b498aa3056fdecab36f079c1881c602110714f
Michael <ko4erov@mail.ru>
2020-02-09 00:58:06 +0300

    cool animation menu icon ready

--- src/blocks/menuIcon/menuIcon.js
+++ src/blocks/menuIcon/menuIcon.js
@@ -5,6 +5,22 @@ const menuElement = document.querySelector('[data-menu]')
 
 menuIconElement.addEventListener('click', () => {
 	// console.log('push')
+	
 	menuElement.classList.toggle('menu--show')
 	menuIconElement.classList.toggle('menu-icon--cross')
+	if (menuIconElement.classList.contains('menu-icon--cross')) {
+
+		menuIconElement.addEventListener('mouseleave', animateMenuIcon)
+	} else {
+		menuIconElement.removeEventListener('mouseleave', animateMenuIcon)
+		menuIconElement.classList.remove('menu-icon--animation')
+	}
+	// setTimeout(() => {
+	// 	// console.log()
+	// 	menuIconElement.classList.toggle('menu-icon--animation')
+	//  },2000)
 })
+
+function animateMenuIcon() {
+	menuIconElement.classList.add('menu-icon--animation')
+}
