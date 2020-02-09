f4e1778b54d0a602b7f510434379d1b0299cc3f9
Michael <ko4erov@mail.ru>
2020-02-09 03:22:42 +0300

    create text person

--- src/blocks/menuIcon/menuIcon.js
+++ src/blocks/menuIcon/menuIcon.js
@@ -1,26 +1,28 @@
 import './menuIcon.scss'
 
-const menuIconElement = document.querySelector('[data-menu-icon]')
-const menuElement = document.querySelector('[data-menu]')
+animateMenuIcon()
 
-menuIconElement.addEventListener('click', () => {
-	// console.log('push')
-	
-	menuElement.classList.toggle('menu--show')
-	menuIconElement.classList.toggle('menu-icon--cross')
-	if (menuIconElement.classList.contains('menu-icon--cross')) {
+// анимация иконки меню-гамбургер
+function animateMenuIcon () {
+	const menuIconElement = document.querySelector('[data-menu-icon]')
+	const menuElement = document.querySelector('[data-menu]')
 
-		menuIconElement.addEventListener('mouseleave', animateMenuIcon)
-	} else {
-		menuIconElement.removeEventListener('mouseleave', animateMenuIcon)
-		menuIconElement.classList.remove('menu-icon--animation')
+	menuIconElement.addEventListener('click', () => {
+		
+		menuElement.classList.toggle('menu--show')
+		menuIconElement.classList.toggle('menu-icon--cross')
+
+		if (menuIconElement.classList.contains('menu-icon--cross')) {
+
+			menuIconElement.addEventListener('mouseleave', animateMenuIconCross)
+		} else {
+			menuIconElement.removeEventListener('mouseleave', animateMenuIconCross)
+			// убираем анимацию крестика по ховеру, когда меню закрыто
+			menuIconElement.classList.remove('menu-icon--animation')
+		}
+	})
+
+	function animateMenuIconCross() {
+		menuIconElement.classList.add('menu-icon--animation')
 	}
-	// setTimeout(() => {
-	// 	// console.log()
-	// 	menuIconElement.classList.toggle('menu-icon--animation')
-	//  },2000)
-})
-
-function animateMenuIcon() {
-	menuIconElement.classList.add('menu-icon--animation')
 }
