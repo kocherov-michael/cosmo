0470fe11e9d811fff446328a061cc2ac380915db
Michael <ko4erov@mail.ru>
2020-02-13 18:35:12 +0300

    input focus behavior

--- src/blocks/input/input.js
+++ src/blocks/input/input.js
@@ -1,1 +1,38 @@
 import './input.scss'
+
+inputBehavior()
+textareaResize()
+
+// поведение инпутов и textarea при фокусе
+function inputBehavior () {
+	const inputList = document.querySelectorAll('[data-input]')
+	for ( let i = 0; i < inputList.length; i++ ) {
+		inputList[i].addEventListener('focus', (event) => {
+			const parentElement = event.currentTarget.closest('label')
+			parentElement.classList.add('label-focus')
+
+			event.currentTarget.addEventListener('blur', (event) => {
+				// если фокус потерян, но введены символы, то текст не опускаем
+				if (!event.currentTarget.value.trim()) {
+					event.currentTarget.value = ''
+					const parentElement = event.currentTarget.closest('label')
+					parentElement.classList.remove('label-focus')
+				}
+			})
+
+		})
+	}
+}
+// прослушка ввода текста в textarea
+function textareaResize () {
+	const textarea = document.querySelector('[data-input="message"]')
+	textarea.addEventListener('keypress', resize)
+}
+
+// увеличение размера textarea по мере заполняемости
+function resize() {
+  setTimeout(() => {
+    this.style.cssText = 'height:auto; padding:0';
+    this.style.cssText = 'height:' + this.scrollHeight + 'px';
+  }, 1);
+}
