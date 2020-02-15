86ed38a04c8f0a219e0ccd03c4b3bb7d5a88887b
Michael <ko4erov@mail.ru>
2020-02-15 20:08:08 +0300

    portfolio page html+style

--- src/blocks/site/site.js
+++ src/blocks/site/site.js
@@ -16,3 +16,25 @@ function hoverSiteBlock () {
 		})
 	}
 }
+
+toggleWebsites ()
+
+function toggleWebsites() {
+	const toggleList = document.querySelectorAll('.portfolio__toggle-item')
+
+	for ( let i = 0; i < toggleList.length; i++ ) {
+		
+		toggleList[i].addEventListener('click', (event) => {
+			for ( let i = 0; i < toggleList.length; i++ ) {
+				toggleList[i].classList.remove('portfolio__toggle-item--choosen')
+
+			}
+			// if (toggleList[i] != event.currentTarget) {
+			// }
+			// const parentElement = buttonList[i].closest('.site__link')
+			// parentElement.style = 'transform: translate(12px, -12px);'
+			event.currentTarget.classList.add('portfolio__toggle-item--choosen')
+		})
+
+	}
+}
