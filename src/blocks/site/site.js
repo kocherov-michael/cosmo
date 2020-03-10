4ce9db09edd1a17bf8fd233a8d2b9cead134b84b
Michael <ko4erov@mail.ru>
2020-03-11 01:42:15 +0300

    add github button to portfolio projects

--- src/blocks/site/site.js
+++ src/blocks/site/site.js
@@ -1,21 +1,21 @@
 import './site.scss'
 
-hoverSiteBlock()
+// hoverSiteBlock()
 
 // задаём стиль родителю при наведении на потомка
-function hoverSiteBlock () {
-	const buttonList = document.querySelectorAll('.site__button')
-	for ( let i = 0; i < buttonList.length; i++ ) {
-		buttonList[i].addEventListener('mouseenter', () => {
-			const parentElement = buttonList[i].closest('.site__link')
-			parentElement.style = 'transform: translate(12px, -12px);'
-		})
-		buttonList[i].addEventListener('mouseleave', () => {
-			const parentElement = buttonList[i].closest('.site__link')
-			parentElement.style = ''
-		})
-	}
-}
+// function hoverSiteBlock () {
+// 	const buttonList = document.querySelectorAll('.site__button')
+// 	for ( let i = 0; i < buttonList.length; i++ ) {
+// 		buttonList[i].addEventListener('mouseenter', () => {
+// 			const parentElement = buttonList[i].closest('.site__link')
+// 			parentElement.style = 'transform: translate(12px, -12px);'
+// 		})
+// 		buttonList[i].addEventListener('mouseleave', () => {
+// 			const parentElement = buttonList[i].closest('.site__link')
+// 			parentElement.style = ''
+// 		})
+// 	}
+// }
 
 toggleWebsites ()
 
